1e1ccca26306843c1f7ebabf295d757e7244a686
TrangPii <22010386@st.phenikaa-uni.edu.vn>
2025-10-23 13:39:21 +0700

    tạo theme.dart thay thế word_piece.dart

---
+++ card_saga/lib/models/theme.dart
@@ -0,0 +1,13 @@
+class GameTheme {
+  final int id;
+  final String name;
+  final int requiredStars;
+  final List<String> cardImages;
+
+  GameTheme({
+    required this.id,
+    required this.name,
+    required this.requiredStars,
+    required this.cardImages,
+  });
+}
--- card_saga/lib/models/world_piece.dart
+++
@@ -1,7 +0,0 @@
-class WorldPiece {
-  final String id;
-  final String country;
-  bool purchased;
-
-  WorldPiece({required this.id, required this.country, this.purchased = false});
-}
